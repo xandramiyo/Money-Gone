d5b3cdedf4a69ba7ab3ed41e54492884fa140075
xandramiyo <aj.sumiyoshi@gmail.com>
2023-05-01 12:54:09 -0700

    formats spending category buttons with Material UI

--- src/pages/Spending/Spending.jsx
+++ src/pages/Spending/Spending.jsx
@@ -2,25 +2,34 @@
 import { Link } from 'react-router-dom'
 import IncomeForm from '../../components/IncomeForm/IncomeForm'
 import IncomeTable from '../../components/IncomeTable/IncomeTable'
+import { Typography, Box, Grid, Button } from '@mui/material';
 
 export default function Spending({ user, incomeEntries, setIncomeEntries, categories }) {
 
     return (
         <>
-            <div className="">
+            <Box className="">
                 <IncomeForm user={user} incomeEntries={incomeEntries} setIncomeEntries={setIncomeEntries}/>
                 <IncomeTable user={user} incomeEntries={incomeEntries} setIncomeEntries={setIncomeEntries}/>
-            </div>
-            <div className="">
-                <Link to="/spending/income-history">
-                    <div className="">Income History</div>
-                </Link>
-                {categories.map((category, index) => 
-                    <Link to="/spending/details" state={{category: category}} key={index} >
-                        <div className="">{category.name}</div>
+            </Box>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid item xs={6}>
+                    <Link to="/spending/income-history">
+                        <Button variant="outlined">Income History</Button>
                     </Link>
+                </Grid>
+                {categories.map((category, index) => 
+                    <Grid item xs={6}>
+                        <Link to="/spending/details" state={{category: category}} key={index} >
+                            <Button 
+                                variant="outlined" 
+                                sx={{ width: '10rem' }}>
+                                    {category.name}
+                            </Button>
+                        </Link>
+                    </Grid>
                 )}
-            </div>
+            </Grid>
         </>
     )
 }
